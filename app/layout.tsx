aed98195765240fb791f49c04d387f4e1ff54194
ky-os <kyle.osunero.21@gmail.com>
2025-12-11 18:28:26 +0800

    feat: Remove VirtualPet component from RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ConvexClientProvider>
-          <VirtualPet />
           {children}
         </ConvexClientProvider>
         <Script
